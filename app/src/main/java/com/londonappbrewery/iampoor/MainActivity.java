ba8aaa98006a23924b9b1d85a99da5df2598e93c
EldarKZ16 <aeldar0016@gmail.com>
2019-02-15 19:08:13 +0600

    Challenge completed!

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -1,8 +1,13 @@
 package com.londonappbrewery.iampoor;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StyleSpan boldStyle = new StyleSpan(Typeface.BOLD);
+        setTextWithSpan((TextView) findViewById(R.id.textView),
+                getString(R.string.i_am_poor),
+                getString(R.string.i_am_poor_bold),
+                boldStyle);
     }
+
+    void setTextWithSpan(TextView textView, String text, String spanText, StyleSpan style) {
+        SpannableStringBuilder sb = new SpannableStringBuilder(text);
+        int start = text.indexOf(spanText);
+        int end = start + spanText.length();
+        sb.setSpan(style, start, end, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        textView.setText(sb);
+    }
+
 }
